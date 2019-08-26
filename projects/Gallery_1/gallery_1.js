a74319054a659fdf18d86e03bb3de8c71ba7d5a3
Wojciech Duma <wojciech.michal.duma@gmail.com>
2019-08-26 19:29:20 +0200

    Add Gallery_1 project

---
+++ projects/Gallery_1/gallery_1.js
@@ -0,0 +1,18 @@
+const showCertificates = () => {
+    const allImg = [...document.querySelectorAll(".container img")];
+    const show = document.querySelector(".show");
+    const showImg = document.querySelector(".show img");
+    allImg.forEach(img => {
+        img.addEventListener("click", () => {
+            show.classList.add("active");
+            const imgSrc = img.getAttribute("src");
+            showImg.setAttribute("src", imgSrc);
+        });
+    });
+
+    show.addEventListener("click", () => {
+        show.classList.remove("active");
+    });
+};
+
+showCertificates();
