342b0f9e444b343d97f4b8dba21d45a297fa0097
Wojciech Duma <wojciech.michal.duma@gmail.com>
2019-08-27 17:40:03 +0200

    Add script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const backgroundEffect = () => {
+    const body = document.querySelector('body');
+
+    body.addEventListener('mousemove', (e) => {
+        const x = e.pageX - e.target.offsetLeft;
+        const y = e.pageY - e.target.offsetTop;
+        e.target.style.setProperty("--x", `${x}px`);
+        e.target.style.setProperty("--y", `${y}px`);
+    });
+};
+
+backgroundEffect();
