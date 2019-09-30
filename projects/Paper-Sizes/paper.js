6ed00b2701ed0801a110a87e38b9bad4e78b581f
Wojciech Duma <wojciech.michal.duma@gmail.com>
2019-09-30 17:54:12 +0200

    Add popup function, add Format-A info

--- projects/Paper-Sizes/paper.js
+++ projects/Paper-Sizes/paper.js
@@ -1,6 +1,26 @@
+const popup = () => {
+  const format__block = document.querySelectorAll(".format__block");
+  const popup__close = document.querySelectorAll(".popup__close");
+
+  // Open popup
+  format__block.forEach(block => {
+    block.addEventListener("click", () => {
+      setTimeout(function() {
+        block.nextElementSibling.classList.add("format__popup--active");
+      }, 300);
+    });
+  });
+
+  // Close popup
+  popup__close.forEach(close => {
+    close.addEventListener("click", () => {
+      close.parentNode.parentNode.classList.remove("format__popup--active");
+    });
+  });
+};
+
 const showClick = () => {
   const format__block = [...document.querySelectorAll(".format__block")];
-
   format__block.forEach(block => {
     block.addEventListener("mousedown", () => {
       block.classList.add("format__block--click");
@@ -12,3 +32,4 @@ const showClick = () => {
 };
 
 showClick();
+popup();
