9e6ccda48df684157aad138d2be1a6bf2924bb80
Wojciech Duma <wojciech.michal.duma@gmail.com>
2020-01-14 19:45:35 +0100

    Add Form Validation - react simple exercise

---
+++ projects/Form/src/App.js
@@ -0,0 +1,203 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    accept: false,
+    messageForm: "",
+
+    error: {
+      username: false,
+      email: false,
+      password: false,
+      accept: false
+    }
+  };
+
+  messages = {
+    username_incorrect:
+      "Nazwa musi być dłuższa niż 8 znaków i nie zawierać spacji.",
+    email_incorrect: "Brak @ w emailu",
+    password_incorrect: "Hasło musi mieć 8 znaków",
+    accept_inncorect: "Nie zaakceptowano regulaminu"
+  };
+
+  handleChange = e => {
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if (type === "text" || type === "password" || type === "email") {
+      const value = e.target.value;
+      this.setState({
+        [name]: value
+      });
+    } else if (type === "checkbox") {
+      const checked = e.target.checked;
+      this.setState({
+        [name]: checked
+      });
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const validation = this.formValidation();
+    console.log(validation);
+
+    if (validation.formCorrect) {
+      this.setState({
+        username: "",
+        email: "",
+        password: "",
+        accept: false,
+        messageForm: "Formularz został wysłany",
+        error: {
+          username: false,
+          email: false,
+          password: false,
+          accept: false
+        }
+      });
+      console.log("Formularz wysłany");
+    } else {
+      this.setState({
+        error: {
+          username: !validation.username,
+          email: !validation.email,
+          password: !validation.password,
+          accept: !validation.accept
+        }
+      });
+      console.log("formularz błędny");
+    }
+  };
+
+  formValidation = () => {
+    // true: okay, false: error
+
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let formCorrect = false;
+
+    if (
+      this.state.username.length > 5 &&
+      this.state.username.indexOf(" ") === -1
+    ) {
+      // this.state.username.indexOf(' ') === -1
+      // jeżeli jest spacja zwróci jakąć cyfre, jak nie będzie to zwróci -1
+      username = true;
+    }
+
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+
+    if (this.state.password.length === 8) {
+      password = true;
+    }
+
+    if (this.state.accept) {
+      accept = true;
+    }
+
+    if (username && email && password && accept) {
+      formCorrect = true;
+    }
+
+    return {
+      formCorrect,
+      username,
+      email,
+      password,
+      accept
+    };
+  };
+
+  // usunięcie wiadomości po 3s
+  componentDidUpdate() {
+    if (this.state.messageForm !== "") {
+      setTimeout(
+        () =>
+          this.setState({
+            messageForm: ""
+          }),
+        3000
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit} noValidate>
+          <label htmlFor="user">
+            Twoje imię:
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            {this.state.error.username && (
+              <span>{this.messages.username_incorrect}</span>
+            )}
+          </label>
+
+          <label htmlFor="email">
+            Twój adres email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+            {this.state.error.email && (
+              <span>{this.messages.email_incorrect}</span>
+            )}
+          </label>
+
+          <label htmlFor="password">
+            Twoje hasło:
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            {this.state.error.password && (
+              <span>{this.messages.password_incorrect}</span>
+            )}
+          </label>
+
+          <label htmlFor="accept">
+            <input
+              type="checkbox"
+              name="accept"
+              id="accept"
+              checked={this.state.accept}
+              onChange={this.handleChange}
+            />
+            Zgadzam się z regulaminem
+            {this.state.error.accept && (
+              <span>{this.messages.accept_inncorect}</span>
+            )}
+          </label>
+
+          <button>Submit</button>
+        </form>
+        {this.state.messageForm && <h3>{this.state.messageForm}</h3>}
+      </div>
+    );
+  }
+}
+
+export default App;
